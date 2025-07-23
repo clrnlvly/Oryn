e6d97795d8e4ca937289a65218db2f8af6ee204e
Lovely Clareon <142940454+clrnlvly@users.noreply.github.com>
2025-07-23 18:50:27 +0800

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,16 +7,23 @@ import authRoutes from "./routes/auth.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// CORS (Frontend origin allowed)
-app.use(cors({
-  origin: "http://localhost:5173", // or "*" if testing only
-}));
+
+if(process.env.NODE_ENV !== "production")
+{
+  // CORS (Frontend origin allowed)
+  app.use(cors({
+    origin: "http://localhost:5173", // or "*" if testing only
+  }));
+}
+
 
 // Body parser
 app.use(express.json());
@@ -27,6 +34,15 @@ app.use(rateLimiter);
 // Routes
 app.use("/api/auth", authRoutes); // ✅ Login
 app.use("/api/notes", notesRoutes); // ✅ Notes (protected)
+
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Connect DB and start server
 connectDB().then(() => {
